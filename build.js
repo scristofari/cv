f2ce47f0b538d77e8a7b9f8a6dbfea8672db5c6f
Sylvain Cristofari <s.cristofari@gmail.com>
2016-07-07 15:07:21 +0200

    Good page size

--- build.js
+++ build.js
@@ -1,13 +1,8 @@
 var page = new WebPage();
 
 page.paperSize = {
-    format        : "A4",
-    orientation    : "portrait",
-    margin        : { left:"1cm", right:"1cm", top:"1cm", bottom:"1cm" },
-};
-page.viewportSize = { 
-    width: 768, 
-    height: 1014 
+  width: '768px',
+  height: '1024px',
 };
 
 page.open("public/index.html", function (status) {
